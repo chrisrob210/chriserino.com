3f1f2e9d3597c8484a16f93687c88d9babfb7b8b
Christopher Robinson <chriserino210@protonmail.com>
2025-07-08 13:06:39 -0500

    added active link styling to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,17 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import { navlinks } from '../constants/navlinks'
 import { Navlink } from '@/lib/definitions'
 
 export default function Navbar() {
-
+    const pathname = usePathname(); // get current path
     return (
-        // <>
-        //     <ul className='flex flex-row justify-center bg-slate-800'>
-        //         {navlinks.map((navlink, index) => (
-        //             <li className='pt-1 px-2'>
-        //                 <Link key={`navlink-${index}`} href={navlink.href}>{navlink.title}</Link>
-        //             </li>
-        //         ))}
-        //     </ul>
-        // </>
         <>
             <ul className='flex flex-row justify-center gap-3 p-1 bg-slate-800'>
                 {navlinks.map((navlink, index) => (
-                    <li key={`nav-ul-li-${navlink.title.toLowerCase()}`} className='whitespace-nowrap'>
+                    <li key={`nav-ul-li-${navlink.title.toLowerCase()}`} className={`whitespace-nowrap ${pathname === navlink.href ? `text-slate-400` : `text-white`}`}>
                         <Link key={`navlink-${navlink.title.toLowerCase()}`} href={navlink.href}>{navlink.title}</Link>
                     </li>
                 ))}
